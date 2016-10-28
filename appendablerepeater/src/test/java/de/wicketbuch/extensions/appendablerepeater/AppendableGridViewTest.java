e78907f15ee45ec9f0e1b7b9ba0043e3753d73ea
Carl-Eric Menzel <cmenzel@wicketbuch.de>
2016-10-28 14:21:01 +0200

    undo split into packages to preserve compatibility with previous release

--- appendablerepeater/src/test/java/de/wicketbuch/extensions/appendablerepeater/AppendableGridViewTest.java
+++ appendablerepeater/src/test/java/de/wicketbuch/extensions/appendablerepeater/AppendableGridViewTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.wicketbuch.extensions.appendablerepeater.gridview;
+package de.wicketbuch.extensions.appendablerepeater;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
